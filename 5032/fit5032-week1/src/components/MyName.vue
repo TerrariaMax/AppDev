4693efce8a54118d4a71d8ada885af1e27bb2cc6
Moana <flowingisland666@gmail.com>
2024-08-02 01:39:35 +1000

    week 1

---
+++ 5032/fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <div class="my-name">
+    <h1>Welcome, Moana!</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+  margin-top: 1rem;
+  font-weight: 400;
+  font-size: 2rem;
+  color: #42b883;
+}
+</style>
