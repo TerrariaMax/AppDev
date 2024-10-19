3b983372e6e655853c51edea68fcac664b2a539d
Moana <flowingisland666@gmail.com>
2024-10-20 02:27:00 +1100

    Trigger deployment

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,30 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base:
+//     process.env.NODE_ENV === 'production'
+//       ? '/AppDev/week4/' // Adjust this path based on your repository and folder structure
+//       : '/',
+//   plugins: [vue(), vueDevTools()],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:
-    process.env.NODE_ENV === 'production'
-      ? '/AppDev/week4/' // Adjust this path based on your repository and folder structure
-      : '/',
-  plugins: [vue(), vueDevTools()],
+  base: '',
+  build: {
+    outDir: 'dist'
+  },
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
